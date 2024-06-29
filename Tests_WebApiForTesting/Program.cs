5394279685e0f12a9dea9a62df669758768e6cb4
Nathaniel Collier <drxs3v3n@gmail.com>
2024-06-29 16:17:40 -0500

    Trying to figure out how to get the swagger gen to work

--- Tests_WebApiForTesting/Program.cs
+++ Tests_WebApiForTesting/Program.cs
@@ -9,13 +9,27 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddControllers();
 
+// swagger
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+app.UseDefaultFiles(); // this will serve Index.html, and other default files.
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.MapControllers();
 app.MapRazorPages();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
+});
+
 
 app.Run();
